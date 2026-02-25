44f2d8fac5ece5b1a4681273c6e362ac0aa8c393
Kevin121602 <kevin.james.gracey@gmail.com>
2026-02-25 13:52:09 -0500

    added fly flag to mt pyre

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1505,7 +1505,7 @@
 #define FLAG_ARRIVED_AT_NAVEL_ROCK                  (SYSTEM_FLAGS + 0x81)
 #define FLAG_LANDMARK_TRAINER_HILL                  (SYSTEM_FLAGS + 0x82)
 
-#define FLAG_UNUSED_0x8E3                           (SYSTEM_FLAGS + 0x83) // Unused Flag
+#define FLAG_VISITED_MT_PYRE                           (SYSTEM_FLAGS + 0x83) // Unused Flag
 
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
